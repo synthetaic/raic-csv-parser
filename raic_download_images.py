199da72717fabb9a58ee9d277517901646eb5aca
brian <brian@synthetaic.com>
2022-11-19 14:15:23 -0600

    revised to deal with new CSV format

--- raic_download_images.py
+++ raic_download_images.py
@@ -9,14 +9,16 @@
 #   dataframe saved as a CSV file, which ties the local filenames back
 #   to the original URL.
 
-import os
 import argparse
 import asyncio
 import csv
+import os
+from os.path import join as pjoin
 from pathlib import Path, PurePath
 from typing import List
 from uuid import uuid4
 
+import numpy as np
 import pandas as pd
 from aiohttp import ClientSession
 
@@ -25,14 +27,13 @@ async def http_get(
     session: ClientSession,
     url: str,
     outfilename: str,
-    sas: str,
     headers: dict = {},
     proxy: str = None,
     timeout: int = 5 * 60,
 ) -> dict:
 
     response = await session.get(
-        url=url + sas, headers=headers, proxy=proxy, timeout=timeout
+        url=url, headers=headers, proxy=proxy, timeout=timeout
     )
     content = None
 
@@ -52,7 +53,6 @@ async def http_get(
 async def http_get_parallel(
     session: ClientSession,
     indf: pd.DataFrame,
-    sas: str,
     headers: dict = {},
     proxy: str = None,
     timeout: int = 5 * 60,
@@ -60,7 +60,7 @@ async def http_get_parallel(
 
     results = await asyncio.gather(
         *[
-            http_get(session, row.Url, row.outfile, sas, headers, proxy, timeout)
+            http_get(session, row.Url, row.outfile, headers, proxy, timeout)
             for k, row in indf.iterrows()
         ]
     )
@@ -70,30 +70,33 @@ async def http_get_parallel(
 async def main(args: argparse.Namespace) -> int:
     csvFile = args.input
     outDir = args.outdir
-    isImagery = args.image_or_video
-    SASkey = f"?{args.saskey}"
+    isImagery = not args.geospatial_data
+    colname = "Url" if args.crops_only else "OriginalImageUrl"
 
     df = pd.read_csv(csvFile)
-    df["Url"] = df["Url"].astype(str)
-    df["outfile"] = df["Url"].apply(
-        lambda x: PurePath(
-            outDir,
-            *list(
-                Path(x).parts[-4:] if not isImagery else [str(uuid4()) + Path(x).suffix]
-            ),
-        )
-    )
-
-    df["outfile"].apply(lambda x: os.makedirs(Path(x).parent, exist_ok=True))
-
+    
+    tmp = df[["FileName",colname]].astype(str)
+    tmp = tmp.rename(columns={colname:"Url"})
+    tmp = tmp.drop_duplicates()
+    
+    if args.crops_only:
+        tmp["CropNum"] = tmp.groupby("FileName").cumcount().astype(str)
+        tmp["outfile"] = tmp.apply(lambda x: (outDir / Path(x.FileName).with_suffix("") / (x.CropNum+".jpg")),axis=1).astype(str) # jpg is the default output from RAIC crops
+        crop_dirs = np.unique(np.array([str(Path(k).parent) for k in tmp.outfile.tolist()]))
+        _ = [os.makedirs(k, exist_ok=True) for k in crop_dirs]
+    else:
+        tmp["outfile"] = [str(outDir / Path(k)) for k in tmp["FileName"].tolist()]
+        _ = [os.makedirs(Path(k).parent, exist_ok=True) for k in tmp["outfile"].tolist()]
+    
     sess = ClientSession()
-    res = await http_get_parallel(sess, df, SASkey)
+    res = await http_get_parallel(sess, tmp)
     saved_csv = str(PurePath(outDir, "ref_" + Path(csvFile).stem + ".csv"))
     await sess.close()
 
     try:
         pd.DataFrame(res).to_csv(saved_csv)
-        print(f"Successfully saved csv to:\n{saved_csv}")
+        print(f"Successfully saved reference dataframe CSV to:\n{saved_csv}")
+        
     except:
         print("Failed to save reference CSV... ignoring...")
         return 1
@@ -111,17 +114,18 @@ if __name__ == "__main__":
         "-o", "--outdir", required=True, help="output directory for downlaoded images"
     )
     parser.add_argument(
-        "-s",
-        "--saskey",
-        required=True,
-        help="SAS Key to access imagery (without the '?')",
+        "--geospatial-data",
+        action="store_true",
+        help="include this flag if the exported CSV contains data from the Maps RAIC module.",
     )
     parser.add_argument(
-        "--image-or-video",
+        "--crops-only",
         action="store_true",
-        help="include this flag if the exported CSV contains data from image or video datasets (not geospatial).",
+        help="Specifies that only the crops (instead of the whole images) from the detections should be downloaded. Otherwise, the whole image is downloaded. This flag is automatically set to false if `--geospatial-data` flag is present.",
     )
 
     args = parser.parse_args()
+    
+    print(args)
 
     asyncio.run(main(args))
